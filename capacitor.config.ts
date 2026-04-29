87befd38a8d6a24483623737ac24e828f2da3f70
sususie-chen <sususie.chen@outlook.com>
2026-04-29 15:48:07 +0800

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nailpolish.app',
+  appName: 'nail-polish-display',
+  webDir: 'dist/public'
+  bundledWebRuntime: false
+};
+
+export default config;
